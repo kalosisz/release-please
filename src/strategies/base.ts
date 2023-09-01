a897cbbd00a6420b5991c782f8e2a67b6cf0ddc0
Szilárd Kálosi <kalosi.szilard@gmail.com>
2023-09-01 15:11:33 +0200

    true  for undefined

--- src/strategies/base.ts
+++ src/strategies/base.ts
@@ -681,7 +681,7 @@ export abstract class BaseStrategy implements Strategy {
   }
 
   isPublishedVersion(version: Version): boolean {
-    return !!this.prerelease || !version.preRelease
+    return (this.prerelease ?? true) || !version.preRelease;
   }
 
   /**
